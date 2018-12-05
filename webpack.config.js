bfabbda291320b54ef22fcf8d6b5ff1f8c37af1f
Nick Shmyrev <nr.shmyrev@gmail.com>
2018-12-05 15:32:32 -0500

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  mode: 'development',
+  mode: isProduction ? 'production' : 'development',
   entry: './src/app.js',
   module: {
     rules: [
@@ -15,14 +18,14 @@ module.exports = {
         test: /\.(s?[ac]ss)$/,
         exclude: /node_modules/,
         use: [
-          'style-loader',
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
           { loader: 'css-loader', options: { sourceMap: true } },
           { loader: 'sass-loader', options: { sourceMap: true } },
         ],
       },
     ],
   },
-  devtool: 'source-map',
+  devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     historyApiFallback: true,
@@ -30,6 +33,11 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'css/style.css',
     }),
   ],
   output: {
